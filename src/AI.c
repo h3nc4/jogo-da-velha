9ffb17f8d4cd83c907038a2ed1b1edcad016e4c4
h3nc4 <me@h3nc4.com>
2023-12-18 14:59:50 -0300

    refactor: lógica comum extraida em nova função

--- src/AI.c
+++ src/AI.c
@@ -28,6 +28,28 @@
 #include "velha.h"
 
 /**
+ * \brief Função auxiliar para iterar sobre o tabuleiro e realizar a lógica comum
+ * \param jogador Valor que representa o jogador
+ * \param melhor_decisao Ponteiro para a melhor decisão
+ * \param jogada Ponteiro para a melhor jogada
+ */
+void melhor_jogada(int jogador, int *melhor_decisao, int *jogada)
+{
+    for (int i = 0; i < 9; i++) // percorre o tabuleiro
+        if (!tabuleiro[i]) // se a posição estiver vazia
+        {
+            tabuleiro[i] = jogador; // preenche a posição com o jogador atual
+            int decisao = -minimax(-jogador); // chama a função minimax com o jogador oposto ao atual
+            tabuleiro[i] = 0; // limpa a posição atual
+            if (decisao > *melhor_decisao) // se o resultado for maior que o conhecido
+            {
+                *melhor_decisao = decisao; // armazena o melhor resultado até o momento
+                *jogada = i; // a melhor jogada se torna a posição atual
+            }
+        }
+}
+
+/**
  * \brief Algoritmo minimax, calcula a melhor jogada possível
  * \return Retorna o jogador vencedor
  * \param jogador Valor que representa o jogador
@@ -36,25 +58,13 @@ int minimax(int jogador)
 {
     int vencedor = vitoria(), // verifica se há um vencedor
         jogada = -1, // jogada = -1 para que qualquer local a sobrescreva
-        decisao, // decisao = resultado da função minimax
         melhor_decisao = -2; // melhor_decisao = -2 para que o primeiro valor o sobrescreva
     if (vencedor) // se houver um vencedor, retorna o jogador atual
         return vencedor * jogador;
-    for (int i = 0; i < 9; i++) // percorre o tabuleiro
-        if (!tabuleiro[i]) // se a posição estiver vazia
-        {
-            tabuleiro[i] = jogador; // preenche a posição com o jogador atual
-            decisao = -minimax(-jogador); // chama a função minimax com o jogador oposto ao atual
-            if (decisao > melhor_decisao) // se o resultado for maior que a saída
-            {
-                melhor_decisao = decisao; // a saída se torna o resultado
-                jogada = i; // a melhor jogada se torna a posição atual
-            }
-            tabuleiro[i] = 0; // limpa a posição atual
-        }
+    melhor_jogada(jogador, &melhor_decisao, &jogada); // itera sobre o tabuleiro
     if (jogada == -1) // se não houver nenhuma jogada possível
         return 0;
-    return melhor_decisao;
+    return melhor_decisao; // retorna a melhor decisão
 }
 
 /**
@@ -63,19 +73,8 @@ int minimax(int jogador)
  */
 int jogada_AI(void)
 {
-    int jogada = -1, melhor_decisao = -2, decisao;
-    for (int i = 0; i < 9; i++) // percorre o tabuleiro
-        if (!tabuleiro[i]) // se a posição não tiver valor
-        {
-            tabuleiro[i] = AI; // preenche a posição com o jogador atual
-            decisao = -minimax(JOGADOR); // chama a função minimax com o jogador oposto da AI
-            tabuleiro[i] = 0; // limpa a posição atual
-            if (decisao > melhor_decisao) // se a jogada for melhor que as anteriores
-            {
-                melhor_decisao = decisao; // a saída se torna o resultado
-                jogada = i; // a melhor jogada se torna a posição atual
-            }
-        }
-    tabuleiro[jogada] = AI;
+    int jogada = -1, melhor_decisao = -2;
+    melhor_jogada(AI, &melhor_decisao, &jogada);
+    tabuleiro[jogada] = AI; // executa a melhor jogada
     return avalia_tabuleiro();
 }
